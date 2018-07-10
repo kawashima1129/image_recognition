39c148d3ea5819cc84791cfa79479a757f039277
kawashima1129 <k.ktak1129@gmail.com>
2018-07-10 22:01:04 +0900

    バグの修正(iou計算など)

--- precision_recall.py
+++ precision_recall.py
@@ -17,48 +17,42 @@ def calc_precision_recall(tp, tp_fp, tp_fn):
     
     return precision, recall
 
-#複数の推定結果がある一つのground truthに対してIOU > 0.5となった場合に
-#IOUが大きい組み合わせのみを抽出
-def method2(iou_list):
-    if len(iou_list) > 1: 
-        
-        dfd = pd.DataFrame(iou_list)
-        dfd_tmp = dfd[dfd.duplicated(subset=1, keep = False)]#ground truthが被ったところ抽出
-        if len(dfd_tmp) == 0:#被っているところがなかったらそのまま元のlistを返す
-            return dfd.values.tolist()
-        
-        iou_max_index = dfd_tmp.idxmax()[2]#列ごとに最大値をとった行をとってくる かつ　IOUの列をとってくる
-        index = set(dfd.index) -  (set(dfd_tmp.index) - set(  list([ iou_max_index ])) )   
+#推定結果とground truthのマッチング
+def adjust(iou_list, result):
 
-        return dfd.ix[index,:].values.tolist()
+    dfd = pd.DataFrame(iou_list)
+    dfd_array = dfd.values
     
-    return iou_list
+    while dfd_array.shape[0] != 0:
+        iou_max_index = dfd.idxmax()[2]
+        tmp = dfd.ix[iou_max_index].values.tolist()
+    
+        flag = False
+        if len(result) > 0:
+            for r in result:
+                if tmp[0] == r[0] or tmp[1] == r[1]:
+                    flag = True
+        if not flag:
+            result.append(tmp)
+        dfd = dfd.drop(iou_max_index)
+        dfd_array = dfd.values
+            
+    return result
 
-#推定結果が複数のground truthに対してIOU > 0.5になった場合に
-#IOUが大きい組み合わせのみ抽出する処理
-def method1(iou_list):
-    #IOU最大値の行だけ取得
-    if len(iou_list) > 1:
-        dfd = pd.DataFrame(iou_list)
-        iou_max_index = dfd.idxmax()[2]#IOUが最大だったindex抽出
-        tmp = dfd.iloc[iou_max_index].values.tolist()#IOUが最大だったレコード抽出
-        return tmp
-    
-    return iou_list
 
 #IOU計算
 def calc_iou(r1, r2):
     xmin1, ymin1, xmax1, ymax1 = r1
     xmin2, ymin2, xmax2, ymax2 = r2
     
-    area1 = (xmax1 - xmin1) * (ymax1 - ymin1)
-    area2 = (xmax2 - xmin2) * (ymax2 - ymin2)
+    area1 = (xmax1 - xmin1 + 1) * (ymax1 - ymin1 + 1)
+    area2 = (xmax2 - xmin2 + 1) * (ymax2 - ymin2 + 1)
     
     and_x1, and_y1 = max(xmin1, xmin2), max(ymin1, ymin2)
     and_x2, and_y2 = min(xmax1, xmax2), min(ymax1, ymax2)
     
-    and_w = and_x2 - and_x1
-    and_h = and_y2 - and_y1
+    and_w = and_x2 - and_x1 + 1
+    and_h = and_y2 - and_y1 + 1
     
     if and_w <= 0 or and_h <= 0:
         return 0
@@ -70,14 +64,13 @@ def calc_iou(r1, r2):
 
 
 def main():
-    pred_path = "./box/"
-    gt_path = "./annotation/"
+    pred_path = "./person/"
+    gt_path = "./correct_data/"
     preds = os.listdir(pred_path)
-    class_num = 3
+    class_num = 1
     classes = [i for i in range(class_num)]
 
     for _class in classes:
-        result_list = []
         tp_list = []
         tp_fp_list = []
         tp_fn_list = []
@@ -98,33 +91,30 @@ def main():
             true_num = true_array.shape[0]#画像１枚あたりのground truthの数
             
             if pred_num != 0:
-                hozon = []
+                iou_list = []
+                result = []
                 for i in range(pred_num):
-                    iou_list = []
                     for j in range(true_num):
                         iou = calc_iou(pred_array[i], true_array[j])
                         if iou >= 0.5:  
                             iou_list.append([i, j, iou])
+                result = adjust(iou_list, result)
 
-                    iou_list = method1(iou_list)
-                    for i in range(len(iou_list)):
-                        hozon.append(iou_list[i])
-
-                result_list = method2(hozon)        
-                tp_list.append(len(result_list))
-                
+            tp_list.append(len(result))
             tp_fp_list.append(pred_num)
             tp_fn_list.append(true_num)
+            if len(result) != true_num or pred_num != true_num:
+                print("誤検出もしくは検出漏れをしたファイル名：", pred)
              
         tp = sum(tp_list)
         tp_fp = sum(tp_fp_list)
         tp_fn = sum(tp_fn_list)
-        #print(tp)
         p, r = calc_precision_recall(tp, tp_fp, tp_fn)
-        
+        #print(tp)        
         print("class = {}, precision = {}, recall = {}".format(_class, p, r))
 
             
     
 if __name__ == "__main__":
     main()
+
